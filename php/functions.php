5f8f5b95cc4e9de1249b90eed4041a77416f5e6a
Harrison <hmcguire13@gmail.com>
2016-09-30 16:45:53 -0700

    php

--- php/functions.php
+++ php/functions.php
@@ -1,11 +1,10 @@
 <?php 
 
 	class  Main {
- 
- 		$json = file_get_contents("../data/votes.json");
-		$data = json_decode($json, true);
 
 		public function insert() {
+			$json = file_get_contents("../data/votes.json");
+			$data = json_decode($json, true);
 			$data['presone']['votes'] = 15;
 			$newjson = json_encode($data);
 			file_put_contents('../data/votes.json', $newjson);

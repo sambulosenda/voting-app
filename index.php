d9f554c98737b5198c94cbf5a0535438978e89bf
Harrison <hmcguire13@gmail.com>
2016-09-30 14:32:08 -0700

    php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 	<div class="container">
 		<?php include('./php/main.php'); ?>
 		<h1>Election Survey</h1>
-		<form action="">
+		<form action="ee.php">
 		    <input type="submit" name="insert" value="insert" onclick="insert()" />
 		    <input type="submit" name="select" value="select" onclick="select()" />
 		</form>
@@ -108,37 +108,3 @@
 </body>
 
 </html>
-
-<?php 
-	if (isset($_POST['action'])) {
-	    switch ($_POST['action']) {
-	        case 'insert':
-	            insert();
-	            break;
-	        case 'select':
-	            select();
-	            break;
-	    }
-	}
-
-	function select() {
-	    echo "The select function is called.";
-	    	echo "Hello world!"; 
-
-			$file = file_get_contents("./data/votes.json");
-
-			$data = file_get_contents("./data/votes.json");
-
-			$json = json_decode(file_get_contents("./data/votes.json"), true);
-
-			echo $data;
-			echo $json;
-	    exit;
-	}
-
-	function insert() {
-	    echo "The insert function is called.";
-	    header('Location: index.php');
-	    exit;
-	}
-?>

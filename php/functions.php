c6a2749928847be0fac9207526950057e65144b6
Harrison <hmcguire13@gmail.com>
2016-09-30 16:43:48 -0700

    php

--- php/functions.php
+++ php/functions.php
@@ -2,10 +2,11 @@
 
 	class  Main {
  
+ 		$json = file_get_contents("../data/votes.json");
+		$data = json_decode($json, true);
+
 		public function insert() {
-			$json = file_get_contents("../data/votes.json");
-			$data = json_decode($json, true);
-			$data['presone']['votes'] = 15;
+			$data['presone']['votes']++;
 			$newjson = json_encode($data);
 			file_put_contents('../data/votes.json', $newjson);
 			header('Location: ../welcome.php');

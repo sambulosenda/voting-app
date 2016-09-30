ec31844bbbf3377c4d12c590c06d37aba5f627d5
Harrison <hmcguire13@gmail.com>
2016-09-30 16:13:17 -0700

    php

--- php/functions.php
+++ php/functions.php
@@ -9,9 +9,26 @@
 		$presOneVotes = $data['presone']['votes'];
  
 		public function insert() {
-			$data['presone']['votes'] = 15;
-			$newJsonString = json_encode($data);
-			file_put_contents('votesd.json', $newJsonString);
+
+		$contents = file_get_contents('./data/votes.json');
+		 
+		//Decode the JSON data into a PHP array.
+		$contentsDecoded = json_decode($contents, true);
+		 
+		//Modify the counter variable.
+		$contentsDecoded['counter']++;
+		 
+		//Encode the array back into a JSON string.
+		$json = json_encode($contentsDecoded);
+		 
+		//Save the file.
+		file_put_contents('./data/votes.json', $json);
+
+
+			
+			// $data['presone']['votes'] = 15;
+			// $newJson = json_encode($data);
+			// file_put_contents('./data/votes.json', $newJson);
 			header('Location: ../welcome.php');
 	    	exit;
 		}

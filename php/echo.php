159507aabf356a9a59425e15b5621f4f9c45560b
Harrison <hmcguire13@gmail.com>
2016-09-30 14:21:04 -0700

    php

--- php/echo.php
+++ php/echo.php
@@ -15,14 +15,14 @@
 	    echo "The select function is called.";
 	    	echo "Hello world!"; 
 
-	$file = file_get_contents("../data/votes.json");
+			$file = file_get_contents("../data/votes.json");
 
-	$data = file_get_contents("./data/votes.json");
+			$data = file_get_contents("../data/votes.json");
 
-	$json = json_decode(file_get_contents("./data/votes.json"), true);
+			$json = json_decode(file_get_contents("../data/votes.json"), true);
 
-	echo $data;
-	echo $json;
+			echo $data;
+			echo $json;
 	    exit;
 	}
 

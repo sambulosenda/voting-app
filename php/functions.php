7c3abcbc2f7143d9e2c01948823fe0f483eb4c13
Harrison <hmcguire13@gmail.com>
2016-09-30 16:46:30 -0700

    php

--- php/functions.php
+++ php/functions.php
@@ -5,7 +5,7 @@
 		public function insert() {
 			$json = file_get_contents("../data/votes.json");
 			$data = json_decode($json, true);
-			$data['presone']['votes'] = 15;
+			$data['presone']['votes']++;
 			$newjson = json_encode($data);
 			file_put_contents('../data/votes.json', $newjson);
 			header('Location: ../welcome.php');

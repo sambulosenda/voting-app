a81219ba098e8d5abb8b3bec6be63191aabdab7d
Harrison <hmcguire13@gmail.com>
2016-09-30 16:23:03 -0700

    php

--- php/functions.php
+++ php/functions.php
@@ -2,33 +2,15 @@
 
 	class  Main {
 
-		$path = "./data/votes.json";
 		$json = file_get_contents("./data/votes.json");
-		$data = json_decode(file_get_contents($path), true);
+		$data = json_decode(file_get_contents($json), true);
 
 		$presOneVotes = $data['presone']['votes'];
  
 		public function insert() {
-
-		$contents = file_get_contents('./data/votes.json');
-		 
-		//Decode the JSON data into a PHP array.
-		$contentsDecoded = json_decode($contents, true);
-		 
-		//Modify the counter variable.
-		$contentsDecoded['presone']['votes']++;
-		 
-		//Encode the array back into a JSON string.
-		$json = json_encode($contentsDecoded);
-		 
-		//Save the file.
-		file_put_contents('./data/votes.json', $json);
-
-
-			
-			// $data['presone']['votes'] = 15;
-			// $newJson = json_encode($data);
-			// file_put_contents('./data/votes.json', $newJson);
+			$data['presone']['votes'] = 15;
+			$newJson = json_encode($data);
+			file_put_contents('./data/votes.json', $newJson);
 			header('Location: ../welcome.php');
 	    	exit;
 		}
--- php/voteone.php
+++ php/voteone.php
@@ -2,8 +2,15 @@
 
 	include './functions.php';
 
-	$main = new Main;
+		$json = file_get_contents("./data/votes.json");
+		$data = json_decode(file_get_contents($json), true);
 
-	$main->insert();
+		$presOneVotes = $data['presone']['votes'];
+ 
+		$data['presone']['votes'] = 15;
+		$newJson = json_encode($data);
+		file_put_contents('./data/votes.json', $newJson);
+		header('Location: ../welcome.php');
+    	exit;
 
 ?>

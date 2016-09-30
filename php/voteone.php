a128edf45b4b75f98818167ca9ca06116afba974
Harrison <hmcguire13@gmail.com>
2016-09-30 16:24:00 -0700

    php

--- php/voteone.php
+++ php/voteone.php
@@ -2,14 +2,15 @@
 
 	include './functions.php';
 
-		$json = file_get_contents("./data/votes.json");
-		$data = json_decode(file_get_contents($json), true);
+		// $json = file_get_contents("./data/votes.json");
+		// $data = json_decode(file_get_contents($json), true);
 
-		$presOneVotes = $data['presone']['votes'];
+		// $presOneVotes = $data['presone']['votes'];
  
-		$data['presone']['votes'] = 15;
-		$newJson = json_encode($data);
-		file_put_contents('./data/votes.json', $newJson);
+		// $data['presone']['votes'] = 15;
+		// $newJson = json_encode($data);
+		// file_put_contents('./data/votes.json', $newJson);
+	
 		header('Location: ../welcome.php');
     	exit;
 

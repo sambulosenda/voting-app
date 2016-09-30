8d946f4f081c7a2339ffcea976971f20cd7c0b78
Harrison <hmcguire13@gmail.com>
2016-09-30 16:25:21 -0700

    php

--- php/functions.php
+++ php/functions.php
@@ -1,16 +1,8 @@
 <?php 
 
 	class  Main {
-
-		$json = file_get_contents("./data/votes.json");
-		$data = json_decode(file_get_contents($json), true);
-
-		$presOneVotes = $data['presone']['votes'];
  
 		public function insert() {
-			$data['presone']['votes'] = 15;
-			$newJson = json_encode($data);
-			file_put_contents('./data/votes.json', $newJson);
 			header('Location: ../welcome.php');
 	    	exit;
 		}
--- php/voteone.php
+++ php/voteone.php
@@ -2,16 +2,8 @@
 
 	include './functions.php';
 
-		// $json = file_get_contents("./data/votes.json");
-		// $data = json_decode(file_get_contents($json), true);
+	$main = new Main;
 
-		// $presOneVotes = $data['presone']['votes'];
- 
-		// $data['presone']['votes'] = 15;
-		// $newJson = json_encode($data);
-		// file_put_contents('./data/votes.json', $newJson);
-	
-		header('Location: ../welcome.php');
-    	exit;
+	$main->insert();
 
 ?>

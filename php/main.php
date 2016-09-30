fe7d170d2b0b29a385f892da5ce414a3c0676741
Harrison <hmcguire13@gmail.com>
2016-09-30 10:43:13 -0700

    php

--- php/main.php
+++ php/main.php
@@ -1,9 +1,14 @@
 <?php 
 	echo "Hello world!"; 
 
-	$file = "../data/votes.json"
+	$file = file_get_contents("../data/votes.json");
 
 	$json = json_decode(file_get_contents($file), true);
+
+echo "var dump"; 
+	var_dump($file);
+
+echo "echo json"; 
 
 	echo $json;
 

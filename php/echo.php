51eb5ca1ae093490c8ed933f0c20c2c1434c7c56
Harrison <hmcguire13@gmail.com>
2016-09-30 15:08:19 -0700

    php

--- php/echo.php
+++ php/echo.php
@@ -1,35 +1,35 @@
 <?php
 
-	// if (isset($_POST['action'])) {
-	//     switch ($_POST['action']) {
-	//         case 'insert':
-	//             insert();
-	//             break;
-	//         case 'select':
-	//             select();
-	//             break;
-	//     }
-	// }
+	if (isset($_POST['action'])) {
+	    switch ($_POST['action']) {
+	        case 'insert':
+	            insert();
+	            break;
+	        case 'select':
+	            select();
+	            break;
+	    }
+	}
 
-	// function select() {
-	//     echo "The select function is called.";
-	//     	echo "Hello world!"; 
+	function select() {
+	    echo "The select function is called.";
+	    	echo "Hello world!"; 
 
-	// 		$file = file_get_contents("../data/votes.json");
+			$file = file_get_contents("../data/votes.json");
 
-	// 		$data = file_get_contents("../data/votes.json");
+			$data = file_get_contents("../data/votes.json");
 
-	// 		$json = json_decode(file_get_contents("../data/votes.json"), true);
+			$json = json_decode(file_get_contents("../data/votes.json"), true);
 
-	// 		echo $data;
-	// 		echo $json;
-	//     exit;
-	// }
+			echo $data;
+			echo $json;
+	    exit;
+	}
 
-	// function insert() {
-	//     echo "The insert function is called.";
-	//     header('Location: ./index.php');
-	//     exit;
-	// }
+	function insert() {
+	    echo "The insert function is called.";
+	    header('Location: ./index.php');
+	    exit;
+	}
 
 ?>

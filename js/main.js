81c06d9bf444b2bc5c343e882b0379ac87c78c7e
Harrison <hmcguire13@gmail.com>
2016-10-02 15:07:56 -0700

    vote two

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
+var	pres1,
+	pres2,
+	pres3,
+	pres4,
+	pres5,
+	pres6,
+	pres7,
+	pres8,
+	pres9;
+
 jsonData();
 
 console.log(pres1);
 console.log(pres2);
 console.log(pres3);
+console.log(pres4);
+console.log(pres5);
+console.log(pres6);
+console.log(pres7);
+console.log(pres8);
+console.log(pres9);
 
 function jsonData() {
 	$.getJSON( "./data/votes.json" ).done( function(votes) {
-		var pres1 = votes.presone.votes;
-		var pres2 = votes.prestwo.votes;
-		var pres3 = votes.presthree.votes;
-		var pres4 = votes.presfour.votes;
-		var pres5 = votes.presfive.votes;
-		var pres6 = votes.pressix.votes;
-		var pres7 = votes.presseven.votes;
-		var pres8 = votes.preseight.votes;
-		var pres9 = votes.presnine.votes;
+		pres1 = votes.presone.votes;
+		pres2 = votes.prestwo.votes;
+		pres3 = votes.presthree.votes;
+		pres4 = votes.presfour.votes;
+		pres5 = votes.presfive.votes;
+		pres6 = votes.pressix.votes;
+		pres7 = votes.presseven.votes;
+		pres8 = votes.preseight.votes;
+		pres9 = votes.presnine.votes;
 	 });
 }

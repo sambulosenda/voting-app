9701f3ed0c13dd5533ae858e15d643a8f673d933
Harrison <hmcguire13@gmail.com>
2016-10-02 16:47:23 -0700

    php

--- php/data.php
+++ php/data.php
@@ -3,12 +3,14 @@
 	$json = file_get_contents("./data/votes.json");
 	$data = json_decode($json, true);
 
-	// $sum = $data['pres1']['votes'] + $data['prestwo']['votes'] + $data['presthree']['votes'] + $data['presfour']['votes'] + $data['presfive']['votes'] + $data['pressix']['votes'] + $data['presseven']['votes'] + $data['preseight']['votes'] + $data['presnine']['votes'];
-
 	$sum = 0;
 
-	for ($i = 1; $i <= 9; $i++) {
-   		 $sum += $data['p' . $i]['votes'];
+	// for ($i = 1; $i <= 9; $i++) {
+ //   		 $sum += $data['p' . $i]['votes'];
+	// }
+
+	foreach ($obj as $p){
+    	$sum += $p->votes;
 	} 
 
 	$totone = round (($data['p1']['votes']/$sum)*100);

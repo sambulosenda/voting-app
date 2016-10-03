e4b5bc0f67a05e5129172ecdc28f704a7818384c
Harrison <hmcguire13@gmail.com>
2016-10-02 17:47:28 -0700

    php

--- php/data.php
+++ php/data.php
@@ -9,8 +9,8 @@
  //   		 $sum += $data['p' . $i]['votes'];
 	// }
 
-	foreach ($data['p'] as $v){
-    	$sum += $v['votes'];
+	foreach ($data['p']['votes'] as $v){
+    	$sum += $v;
 	} 
 
 	foreach ($data as $v){

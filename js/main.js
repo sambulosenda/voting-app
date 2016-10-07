67e885cea5dfa8290ed6fc040a9cf8d30547d9e5
Harrison <hmcguire13@gmail.com>
2016-10-06 21:49:02 -0700

    php sort

--- js/main.js
+++ js/main.js
@@ -37,16 +37,16 @@ function indexOfMax(arr) {
 }
 
 function jsonData() {
-	$.getJSON( "./data/votes.json" ).done( function(votes) {
-		p1 = votes.p1.votes;
-		p2 = votes.p2.votes;
-		p3 = votes.p3.votes;
-		p4 = votes.p4.votes;
-		p5 = votes.p5.votes;
-		p6 = votes.p6.votes;
-		p7 = votes.p7.votes;
-		p8 = votes.p8.votes;
-		p9 = votes.p9.votes;
+	$.getJSON( "./data/votes.json" ).done( function(presidents) {
+		p1 = presidents[0].votes;
+		p2 = presidents[1].votes;
+		p3 = presidents[2].votes;
+		p4 = presidents[3].votes;
+		p5 = presidents[4].votes;
+		p6 = presidents[5].votes;
+		p7 = presidents[6].votes;
+		p8 = presidents[7].votes;
+		p9 = presidents[8].votes;
 
 		tarray = [p1, p2, p3, p4, p5, p6, p7, p8, p9];
 		winner = indexOfMax(tarray);

09b9ddb93c321c228f06f070fddf83e603875dd1
Harrison <hmcguire13@gmail.com>
2016-10-06 21:27:35 -0700

    php sort

--- php/main.php
+++ php/main.php
@@ -2,18 +2,17 @@
 
 	$json = file_get_contents("../data/votes.json");
 	$data = json_decode($json, true);
-	$info = $data['presidents'];
 
 	class  Main {
 
 		public function vote($p) {
-			global $info;
-			$info[$p]['votes']++;
-			$newjson = json_encode($info);
+			global $data;
+			$data['presidents'][$p]['votes']++;
+			$newjson = json_encode($data);
 			file_put_contents('../data/votes.json', $newjson);
 			
 			$cookie_name = "election";
-			$cookie_value = $info[$p]['name'];
+			$cookie_value = $data[$p]['name'];
 			setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
 
 			header('Location: ../results.php');

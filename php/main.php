3fab5bce1916d58fb6dce73d1ae213c92f8f7845
Harrison <hmcguire13@gmail.com>
2016-10-11 17:49:18 -0700

    658px width design edits

--- php/main.php
+++ php/main.php
@@ -7,16 +7,16 @@
 
 		public function vote($p) {
 			global $data;
+
+			$cookie_name = "election";
+			$cookie_value = $data['presidents'][$p]['name'];
+			setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+
 			$data['presidents'][$p]['votes']++;
 			$newjson = json_encode($data);
 			file_put_contents('../data/votes.json', $newjson);
-			
-			$cookie_name = "election";
-			$cookie_value = $data['presidents'][$p]['name'];
 
 			header('Location: ../results.php');
-			
-			setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
 
 	    	exit;
 
